be7748994a8f3c9b448d4814f57e62b1b046cd97
darkfloid6 <darkfloid6@gmail.com>
2021-01-24 18:25:21 +0100

    please

--- main.js
+++ main.js
@@ -13,4 +13,19 @@ c.moveTo(20, 20);
 c.lineTo(100,100);
 c.lineTo(100, 30);
 c.closePath();
+c.strokeStyle = 'blue';
 c.stroke();
+
+//circles
+
+for (var i = 0; i < 3; i++) {
+  var red = Math.floor(Math.random()* 255);
+var green = Math.floor(Math.random() * 255);
+var blue = Math.floor(Math.random() * 255);
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+c.beginPath();
+c.arc(x, y, 50, 30, 50, false);
+c.strokeStyle = "rgb("+red+","+green+"," +blue+" )";
+c.stroke();
+};
