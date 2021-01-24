d81d2058d9f6a5f6c4031937dac1c3e956dc9cdb
darkfloid6 <darkfloid6@gmail.com>
2021-01-25 00:30:29 +0100

    msg

--- main.js
+++ main.js
@@ -29,40 +29,61 @@ c.arc(x, y, 50, 30, 50, false);
 c.strokeStyle = "rgb("+red+","+green+"," +blue+" )";
 c.stroke();
 };*/
-function Circle(x, y) {
+
+
+
+function Circle(x, y, dx, dy, radius) {
   // body...
   this.x = x;
   this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
   
-  this.draw = function() {
-    console.log('Stephen');
+  this.draw = function(){
+c.beginPath();
+c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+c.strokeStyle = 'yellow';
+c.stroke();
+  }
+  
+  this.update = function () {
+    // body...
+if (this.x + this.radius > innerWidth || this.x - this.radius < 0){
+  this.dx = -this.dx;
+}
+if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+  this.dy = -this.dy;
+}
+this.x += this.dx;
+this.y += this.dy;
+
+this.draw();
   }
 }
 
-var circle = new Circle(100, 100);
-circle.draw();
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 8;
-var dy = (Math.random() - 0.5) * 8;
-var radius = 30;
+var circleArray = [];
+
+for (var i = 0; i < 20; i++) {
+  var x = Math.random() * (innerWidth - radius * 2) + radius;
+  var y = Math.random() * (innerHeight - radius * 2) + radius;
+  var dx = (Math.random() - 0.5) * 8;
+  var dy = (Math.random() - 0.5) * 8;
+  var radius = 30
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+console.log(circleArray);
 
 function animate() {
   // body...
   requestAnimationFrame(animate);
 c.clearRect(0, 0, innerWidth, innerHeight);
-c.beginPath();
-c.arc(x, y, radius, Math.PI * 2, false);
-c.strokeStyle = 'yellow';
-c.stroke();
-if (x + radius > innerWidth || x - radius < 0){
-  dx = -dx;
+
+for (var i = 0; i < circleArray.length; i++) {
+  circleArray[i].update();
 }
-if (y + radius > innerHeight || y - radius < 0) {
-  dy = -dy;
-}
-x += dx;
-y += dy;
+
 };
 animate();
